f8ad30dfcf1fd6c013c8c7b46dda42a450cf3c1b
Brandon Kan <bkan0458@gmail.com>
2023-03-25 21:10:09 -0600

    framer motion added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,12 +13,27 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#914eff]'>Brandon Kan</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I Develop CRM, and Power Platform applications</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I Develop CRM, Azure and Power Platform applications</p>
 
 
         </div>
       </div>
       <ComputersCanvas /> 
+       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <motion.dev
+  animate={{ y: [0,24,0] }}
+  transition= {{
+    duration: 1.5,
+    repeat: Infinity,
+    repeatType: 'loop'
+  }}
+  className="w-3 h-3 rounded-full bg-secondary mb-1"
+/>
+          </div>
+        </a>
+       </div>
     </section>
   )
 }
